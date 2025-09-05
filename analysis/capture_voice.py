b43be17dde2e4db9c5f4436e68239140ca924ddd
Clouds Flowing <clouds.flowing@gmail.com>
2025-09-05 20:50:29 +0800

    add plot_input

--- analysis/capture_voice.py
+++ analysis/capture_voice.py
@@ -8,7 +8,7 @@ import threading
 # %%
 CHANNELS = 1
 RATE = 44100  # 采样率
-CHUNK = 1024  # 每次读取的音频块大小
+CHUNK = 4096  # 每次读取的音频块大小
 RECORD_SECONDS = 0  # 0表示无限录制
 
 # %%
@@ -26,8 +26,8 @@ def detect_pitch(audio_data: np.ndarray, sr=RATE) -> float | None:
   audio_data = audio_data / max_abs
   f0, _, _ = librosa.pyin(
       audio_data,
-      fmin=librosa.note_to_hz('C2'),  # 最低检测音高 65hz value=36
-      fmax=librosa.note_to_hz('C7'),  # 最高检测音高 2093hz value=96
+      fmin=librosa.note_to_hz('D3'),  # 最低检测音高 65hz value=36
+      fmax=librosa.note_to_hz('A5'),  # 最高检测音高 2093hz value=96
       sr=sr
   )
   if f0 is not None:
@@ -54,33 +54,20 @@ def hz_to_note(frequency: float) -> str:
 def describe_np(array: np.ndarray) -> str:
   return f"shape: {array.shape}, dtype: {array.dtype}, min: {np.min(array)}, max: {np.max(array)}, mean: {np.mean(array):.4f}, std: {np.std(array):.4f}"
 
-def handle_frame(stream: sd.InputStream):
+def handle_frame(stream: sd.InputStream, chunk_size = None):
   # read returns (frames, overflow)
+  global CHUNK
   frames, overflow = stream.read(CHUNK)
   if overflow:
-    # overflow is a boolean array or flag; if True, skip this block
-    # print("缓冲区溢出，跳过此帧", frames)
-    return None
-  print(describe_np(frames))
+    print("缓冲区溢出，增加块大小", stream.read_available, overflow)
+    CHUNK *= 2
+  # if frames.any(): print(describe_np(frames))
   # frames shape: (CHUNK, CHANNELS) -> squeeze to 1D for mono
   audio_data = np.squeeze(np.array(frames, dtype=np.float32))
   # 检测音高
   pitch = detect_pitch(audio_data)
   # 显示结果
-  if pitch:
-    midi = librosa.hz_to_midi(pitch)
-    note = librosa.hz_to_note(pitch)
-    print(f"当前音高: {midi:.2f} MIDI, {note}")
-    pitch_data = {
-        "midi": midi,
-        "note": note,
-        "pitch": pitch,
-    }
-    global latest_pitch_data
-    with pitch_lock:
-      latest_pitch_data = pitch_data
-  else:
-    print("未检测到有效音高...")
+  return pitch
 
 def audio_processing_thread():
   start_time = time.time()
@@ -89,13 +76,27 @@ def audio_processing_thread():
     # Use sounddevice InputStream which is usually easier to install on macOS
     with sd.InputStream(samplerate=RATE, channels=CHANNELS, dtype='float32', blocksize=CHUNK) as stream:
       while is_running:
-        handle_frame(stream)
+        pitch = handle_frame(stream)
+
+        if pitch:
+          midi = librosa.hz_to_midi(pitch)
+          note = librosa.hz_to_note(pitch)
+          print(f"当前音高: {midi:.2f} MIDI, {note}")
+          pitch_data = {
+              "midi": midi,
+              "note": note,
+              "pitch": pitch,
+          }
+          global latest_pitch_data
+          with pitch_lock:
+            latest_pitch_data = pitch_data
         # 短暂延迟，减少CPU占用
         time.sleep(0.05)
   except Exception as e:
     print(f"检测已停止: {e}")
   finally:
     print("音频处理线程已退出")
+
 # %%
 from flask import Flask, jsonify
 
@@ -121,6 +122,8 @@ def run_http_server(port=8000, host='localhost'):
 
 # %%
 def main():
+  print(sd.query_devices())
+  print('default device:', sd.default.device)
   audio_thread = threading.Thread(target=audio_processing_thread, daemon=True)
   audio_thread.start()
 
